7d9b6568bdb1fa2c09afc1190c4032f18a8bc589
Janith08 <kjanith08@gmail.com>
2022-11-20 22:03:54 +0530

    Second commit

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './formInput.css'
 
 const FormInput = (props) => {
+    const {label, onChange, id, ...inputProps}  = props;
   return (
     <div className='formInput'>
-        <input placeholder= {props.placeholder} onChange={e=>props.Username(e.target.value)} />
+        <label>{label}</label>
+        <input {...inputProps} onChange={onChange} />
     </div>
   )
 }
