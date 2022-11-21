195bb8e5271805a25d6c61adb534bdf1039eed75
Janith08 <kjanith08@gmail.com>
2022-11-21 18:01:15 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,93 @@
-
-import { useState } from 'react';
-import './app.css'
-import FormInput from './components/FormInput';
+import { useState } from "react";
+import "./app.css";
+import FormInput from "./components/FormInput";
 
 const App = () => {
-   const [values, setValues] = useState({
-     username: "",
-     email: "",
-     birthday: "",
-     password: "",
-     confirmPassword: ""
-   });
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-   const inputs = [
-     {
-       id: 1,
-       name: "username",
-       type: "text",
-       placeholder: "Username",
-       errorMessage:
-         "Username would be 3-16 characters and shouldn't include any special characters",
-       label: "Username",
-     },
-     {
-       id: 2,
-       name: "email",
-       type: "text",
-       placeholder: "Email",
-       errorMessage: "It should be a valid Email address",
-       label: "Email",
-     },
-     {
-       id: 3,
-       name: "birthday",
-       type: "date",
-       placeholder: "Birthday",
-       label: "Birthday",
-     },
-     {
-       id: 4,
-       name: "password",
-       type: "password",
-       placeholder: "Password",
-       errorMessage: "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character",
-       label: "Password",
-     },
-     {
-       id: 5,
-       name: "confirmPassword",
-       type: "password",
-       placeholder: "Confirm Password",
-       errorMessage: "Password didn't match",
-       label: "Confirm Password",
-     },
-   ];
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username would be 3-16 characters and shouldn't include any special characters",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid Email address",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character",
+      label: "Password",
+      pattern:
+        "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,20}$", //Regex code
+      required: true,
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "Password didn't match",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
+  ];
 
-    const handleSubmit = (e) => {
-        e.preventDefault();    
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    const onChange = (e) => {
-        setValues({...values, [e.target.name]: e.target.value })
-    }
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
-    console.log(values)
-    return (
-      <div className="app">
-        <form onSubmit={handleSubmit}>
-          <h1>Register</h1>
-          {inputs.map((input) => (
-            <FormInput
-              key={input.id}
-              {...input}
-              value={values[input.name]}
-              onChange={onChange}
-            />
-          ))}
-          <button>Submit</button>
-        </form>
-      </div>
-    );
-    
+  console.log(values);
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
